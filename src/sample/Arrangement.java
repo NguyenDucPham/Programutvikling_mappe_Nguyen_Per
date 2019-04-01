4329b6c3a6fb91aec3316faa69122fc9df05927f
perhje <38944777+perhje@users.noreply.github.com>
2019-04-01 13:00:37 +0200

    Arrangement og billett, grunnleggende

--- src/sample/Arrangement.java
+++ src/sample/Arrangement.java
@@ -1,4 +1,78 @@
 package sample;
 
+
 public class Arrangement {
+     String type;
+     String navn;
+     String artister;
+     String program;
+     String tidspunkt;
+     double pris;
+     String billetter;
+
+    public Arrangement(String type, String navn, String artister, String program, String tidspunkt, double pris, String billetter) {
+        this.type = type;
+        this.navn = navn;
+        this.artister = artister;
+        this.program = program;
+        this.tidspunkt = tidspunkt;
+        this.pris = pris;
+        this.billetter = billetter;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public String getArtister() {
+        return artister;
+    }
+
+    public void setArtister(String artister) {
+        this.artister = artister;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public String getTidspunkt() {
+        return tidspunkt;
+    }
+
+    public void setTidspunkt(String tidspunkt) {
+        this.tidspunkt = tidspunkt;
+    }
+
+    public double getPris() {
+        return pris;
+    }
+
+    public void setPris(double pris) {
+        this.pris = pris;
+    }
+
+    public String getBilletter() {
+        return billetter;
+    }
+
+    public void setBilletter(String billetter) {
+        this.billetter = billetter;
+    }
 }
--- src/sample/Billett.java
+++ src/sample/Billett.java
@@ -1,4 +1,57 @@
 package sample;
 
 public class Billett {
+    String plass;
+    double pris;
+    String nummer;
+    String dato;
+    String klokkeslett;
+
+    public Billett(String plass, double pris, String nummer, String dato, String klokkeslett) {
+        this.plass = plass;
+        this.pris = pris;
+        this.nummer = nummer;
+        this.dato = dato;
+        this.klokkeslett = klokkeslett;
+    }
+
+    public String getPlass() {
+        return plass;
+    }
+
+    public void setPlass(String plass) {
+        this.plass = plass;
+    }
+
+    public double getPris() {
+        return pris;
+    }
+
+    public void setPris(double pris) {
+        this.pris = pris;
+    }
+
+    public String getNummer() {
+        return nummer;
+    }
+
+    public void setNummer(String nummer) {
+        this.nummer = nummer;
+    }
+
+    public String getDato() {
+        return dato;
+    }
+
+    public void setDato(String dato) {
+        this.dato = dato;
+    }
+
+    public String getKlokkeslett() {
+        return klokkeslett;
+    }
+
+    public void setKlokkeslett(String klokkeslett) {
+        this.klokkeslett = klokkeslett;
+    }
 }
