a3fdc7dc67c772632e1a66cc86eb0d24e3b34b09
Nguyen pham <phamducnguyen@hotmail.com>
2019-04-07 10:39:02 +0200

    ryddet opp shiten

--- src/sample/KontaktPerson.java
+++ src/sample/KontaktPerson.java
@@ -1,4 +1,62 @@
 package sample;
 
 public class KontaktPerson {
+
+    private String Navn;
+    private String TelefonNummer;
+    private String Email;
+    private String Ekstraopplysning;
+    private String Nettside;
+    private String Firma;
+
+    public String getNavn() {
+        return Navn;
+    }
+
+    public void setNavn(String navn) {
+        Navn = navn;
+    }
+
+    public String getTelefonNummer() {
+        return TelefonNummer;
+    }
+
+    public void setTelefonNummer(String telefonNummer) {
+        TelefonNummer = telefonNummer;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getEkstraopplysning() {
+        return Ekstraopplysning;
+    }
+
+    public void setEkstraopplysning(String ekstraopplysning) {
+        Ekstraopplysning = ekstraopplysning;
+    }
+
+    public String getNettside() {
+        return Nettside;
+    }
+
+    public void setNettside(String nettside) {
+        Nettside = nettside;
+    }
+
+    public String getFirma() {
+        return Firma;
+    }
+
+    public void setFirma(String firma) {
+        Firma = firma;
+    }
+
+
+
 }
--- src/sample/Lokale.java
+++ src/sample/Lokale.java
@@ -1,4 +1,37 @@
 package sample;
 
 public class Lokale {
+
+    private String LokalNavn;
+    //skal være kino, konsertsal, foredragssal, teatersal
+    private String LokalType;
+    private int AntallPlasser;
+
+
+    public String getLokalNavn() {
+        return LokalNavn;
+    }
+
+    public void setLokalNavn(String lokalNavn) {
+        LokalNavn = lokalNavn;
+    }
+
+    public String getLokalType() {
+        return LokalType;
+    }
+
+    public void setLokalType(String lokalType) {
+        LokalType = lokalType;
+    }
+
+    public int getAntallPlasser() {
+        return AntallPlasser;
+    }
+
+    public void setAntallPlasser(int antallPlasser) {
+        AntallPlasser = antallPlasser;
+    }
+
+
+
 }
