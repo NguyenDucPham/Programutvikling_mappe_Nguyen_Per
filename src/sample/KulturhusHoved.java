0dde6a9547425c2592543f3d81bb3328c5c1b438
Nguyen pham <phamducnguyen@hotmail.com>
2019-04-08 11:42:07 +0200

    ryddet opp shiten

--- src/sample/KulturhusHoved.java
+++ src/sample/KulturhusHoved.java
@@ -2,26 +2,32 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 
 public class KulturhusHoved extends Application {
 //Pham sin del
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("hovedside.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Tab.fxml"));
         primaryStage.setTitle("Kulturhus");
-        primaryStage.setScene(new Scene(root, 300, 275));
+       // GridPane gridPane = RegistreringLokale();
+        Scene scene = new Scene(root, 800, 500);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
     }
 }
-
 
 
 
