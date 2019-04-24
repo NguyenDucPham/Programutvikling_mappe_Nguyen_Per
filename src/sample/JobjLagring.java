9fbf232d424ae5e70c0554713c4ef84c722f621e
perhje <38944777+perhje@users.noreply.github.com>
2019-04-24 14:40:59 +0200

    Fillagrer. Jobjlagring

--- src/sample/JobjLagring.java
+++ src/sample/JobjLagring.java
@@ -1,17 +1,24 @@
 package sample;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class JobjLagring extends FilLagring{
     @Override
     public void skriver(Object object, String filPlassering) throws IOException {
-        PrintWriter skriver;
-        skriver = new PrintWriter(filPlassering+".jobj","UTF-8");
-        skriver.println(object);
+        try(FileWriter filSkriver = new FileWriter(filPlassering+".jobj", true);
+            BufferedWriter bufferSkriver = new BufferedWriter(filSkriver);
+            PrintWriter skriver = new PrintWriter(bufferSkriver))
+        {
+            skriver.println(object.toString());
 
-        if(skriver != null){
-            skriver.close();
+            if(skriver != null){
+                skriver.close();
+            }
+        } catch (IOException e) {
+
         }
     }
 }
