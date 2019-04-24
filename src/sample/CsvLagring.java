7ee1d8a1e394ee6d6dc1581af9f6706e0f4f05e7
perhje <38944777+perhje@users.noreply.github.com>
2019-04-24 14:17:12 +0200

    Fillagrer. Csvlagring

--- src/sample/CsvLagring.java
+++ src/sample/CsvLagring.java
@@ -1,18 +1,21 @@
 package sample;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class CsvLagring extends FilLagring{
     @Override
-    public void skriver(Object object, String filPlassering) throws IOException {
-        PrintWriter skriver;
-        skriver = new PrintWriter(filPlassering+".csv","UTF-8");
-        for(object)
-        skriver.println(object);
+    public void skriver(Object object, String filPlassering){
+        try(FileWriter filSkriver = new FileWriter(filPlassering+".csv", true);
+            BufferedWriter bufferSkriver = new BufferedWriter(filSkriver);
+            PrintWriter skriver = new PrintWriter(bufferSkriver))
+        {
+            skriver.println(object.toString());
 
-        if(skriver != null){
-            skriver.close();
+            if(skriver != null){
+                skriver.close();
+            }
+        } catch (IOException e) {
+
         }
     }
 }
