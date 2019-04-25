7b3feb4dde3cd314d4edfd471f0a4e3ef2351184
perhje <38944777+perhje@users.noreply.github.com>
2019-04-25 12:57:56 +0200

    Fillagrer. Jobjlagring

--- src/sample/CsvLagring.java
+++ src/sample/CsvLagring.java
@@ -5,15 +5,12 @@ import java.io.*;
 public class CsvLagring extends FilLagring{
     @Override
     public void skriver(Object object, String filPlassering){
-        try(FileWriter filSkriver = new FileWriter(filPlassering+".csv", true);
+        try {
+            FileWriter filSkriver = new FileWriter(filPlassering+".csv", true);
             BufferedWriter bufferSkriver = new BufferedWriter(filSkriver);
-            PrintWriter skriver = new PrintWriter(bufferSkriver))
-        {
+            PrintWriter skriver = new PrintWriter(bufferSkriver);
             skriver.println(object.toString());
-
-            if(skriver != null){
-                skriver.close();
-            }
+            skriver.close();
         } catch (IOException e) {
 
         }
--- src/sample/JobjLagring.java
+++ src/sample/JobjLagring.java
@@ -8,15 +8,12 @@ import java.io.PrintWriter;
 public class JobjLagring extends FilLagring{
     @Override
     public void skriver(Object object, String filPlassering) throws IOException {
-        try(FileWriter filSkriver = new FileWriter(filPlassering+".jobj", true);
+        try {
+            FileWriter filSkriver = new FileWriter(filPlassering+".jobj", true);
             BufferedWriter bufferSkriver = new BufferedWriter(filSkriver);
-            PrintWriter skriver = new PrintWriter(bufferSkriver))
-        {
+            PrintWriter skriver = new PrintWriter(bufferSkriver);
             skriver.println(object.toString());
-
-            if(skriver != null){
-                skriver.close();
-            }
+            skriver.close();
         } catch (IOException e) {
 
         }
